598ec205284e0ce083ace07fce62777760f07d45
Srihari <runnersunny07@gmail.com>
2026-09-25 21:30:16 +0530

    first commit

---
+++ my-project/src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <div className='flex justify-center text-center mt-46 text-4xl font-bold bg-amber-200 m-4'>
+      Hello Developers, This is Devops Pratice Project (DevSecOps) Project
+    </div>
+  )
+}
+
+export default App
